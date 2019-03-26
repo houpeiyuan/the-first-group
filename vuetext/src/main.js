047dcc21f4929a0ed8d5c9d18441f1d6b7de4a45
SC-201811221643\lanou <1241561194@qq.com>
2019-03-26 11:56:34 +0800

    王加利

--- vuetext/src/main.js
+++ vuetext/src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/index'
-
 import 'swiper/dist/css/swiper.css'
-
 import './assets/reset.css'
 Vue.config.productionTip = false
+import mint from 'mint-ui'
+import '../node_modules/mint-ui/lib/style.css'
+Vue.use(mint)
 new Vue({
   el: '#app',
   router,
