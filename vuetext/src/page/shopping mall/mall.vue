ade2df0f65f12caf15b05baea935eb259e5e32be
SC-201811221643\lanou <1241561194@qq.com>
2019-04-01 17:43:04 +0800

    123

---
+++ vuetext/src/page/shopping mall/mall.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="box">
+      <h2>请重新进入</h2>
+      <br>
+      <p>请返回重新进入积分商城</p>
+      <img src="http://img4.cache.netease.com/photo/0008/2017-04-19/CIDBSLB6843T0008.jpg" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "mall"
+    }
+</script>
+
+<style scoped>
+.box{
+  text-align: center;
+  margin-top: 1.5rem;
+}
+img{
+  width: 2rem;
+  border-radius: 50%;
+  margin-top: 0.3rem;
+}
+</style>
